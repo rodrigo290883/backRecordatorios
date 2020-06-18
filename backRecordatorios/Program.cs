45ec1cfe4068777b910849088cbc49f8c227d359
Rodrigo Lira <rodrigo290883@gmail.com>
2020-06-17 23:21:22 -0500

    commit inicial

---
+++ backRecordatorios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace backRecordatorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
